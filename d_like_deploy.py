6523d9db99a5594e72996516e29429986c8d5178
Marek Żochowski <marrkzochowski2@gmail.com>
2022-05-16 18:33:33 +0200

    Task 1.3

--- d_like_deploy.py
+++ d_like_deploy.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response
 
 app = FastAPI()
 
@@ -31,3 +31,20 @@ def method_put():
 @app.options("/method")
 def method_options():
     return {"method": "OPTIONS"}
+
+
+@app.get("/day")
+def validate_day_number(name: str, number: int):
+    days = {
+        "monday": 1,
+        "tuesday": 2,
+        "wednesday": 3,
+        "thursday": 4,
+        "friday": 5,
+        "saturday": 6,
+        "sunday": 7,
+    }
+
+    if (name in days) and (days[name] == number):
+        return Response(status_code=200)
+    return Response(status_code=400)
