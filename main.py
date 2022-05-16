fa47db4cff5a16d630a3276412b5888edc4f6e83
Marek Żochowski <marrkzochowski2@gmail.com>
2022-05-16 21:30:21 +0200

    Uncomment local testing

--- main.py
+++ main.py
@@ -1,3 +1,2 @@
 class HerokuApp:
-    #app_url = "https://d-jak-deployment-marek.herokuapp.com/"
-    app_url = "http://localhost:8000"
+    app_url = "https://d-jak-deployment-marek.herokuapp.com/"
