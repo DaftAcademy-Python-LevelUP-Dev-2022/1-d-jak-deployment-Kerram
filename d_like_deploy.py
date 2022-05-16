ad21fa98493680cd1601a61261d6e957b9b7facb
Marek Żochowski <marrkzochowski2@gmail.com>
2022-05-16 17:35:47 +0200

    Task 1.2

--- d_like_deploy.py
+++ d_like_deploy.py
@@ -4,5 +4,30 @@ app = FastAPI()
 
 
 @app.get("/")
-async def root():
+def root():
     return {"start": "1970-01-01"}
+
+
+@app.get("/method")
+def method_get():
+    return {"method": "GET"}
+
+
+@app.post("/method", status_code=201)
+def method_post():
+    return {"method": "POST"}
+
+
+@app.delete("/method")
+def method_delete():
+    return {"method": "DELETE"}
+
+
+@app.put("/method")
+def method_put():
+    return {"method": "PUT"}
+
+
+@app.options("/method")
+def method_options():
+    return {"method": "OPTIONS"}
